0b0482ed0dbe55e1612471142570f6bf79562aab
goloneczka <48791689+goloneczka@users.noreply.github.com>
2022-07-18 14:10:02 +0200

    init commit for web server app

---
+++ domain/src/test/java/server/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package server.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
